253b66f9696fa4fbae1c33ff529f5d0472c1ff88
Zhengyang Zhang <zzhan127@ucsc.edu>
2020-09-13 06:40:39 -0700

    first commit

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+	
+	public CharacterController controller;
+	public float speed = 6f;
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+		float vertical = Input.GetAxisRaw("Vertical");
+		
+		Vector3 direction = new Vector3(horizontal,0f,vertical).normalized;
+		
+		if(direction.magnitude >=0.1f){
+			float targetAngle = Mathf.Atan2(direction.x,direction.y) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.Euler(0f,targetAngle,0f);
+			controller.Move(direction * speed * Time.deltaTime);
+		}
+		
+    }
+}
